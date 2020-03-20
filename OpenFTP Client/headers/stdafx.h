feb0a94e1fd25e201c3dc13a92d550cfce8edbd6
kandabi <gatefighter@gmail.com>
2020-03-20 15:32:35 +0200

    Solution can now compile for 32x or 64x architecures

--- OpenFTP Client/headers/stdafx.h
+++ OpenFTP Client/headers/stdafx.h
@@ -3,7 +3,7 @@
 #include <utility>
 #include <QSound>
 
-#define APP_VERSION "0.2.9" 
+#define APP_VERSION "0.3.0" 
 
 #ifdef QT_DEBUG 
 #define CRYPTO_KEY 0xb2f641dc9fffec4e //*** Randomize this on each new release version
--- OpenFTP Server/headers/stdafx.h
+++ OpenFTP Server/headers/stdafx.h
@@ -3,7 +3,7 @@
 #include <QTcpServer>
 #include <qnetworkinterface>
 
-#define APP_VERSION "0.2.9" 
+#define APP_VERSION "0.3.0" 
 
 #ifdef QT_DEBUG
 #define CRYPTO_KEY 0x3cdd48c744c19481 //*** Randomize this on each new release version
